f44bd869028469e9832800bee91e36f62ac1a206
Miry Konishi <miry.konishi@gmail.com>
2023-03-13 21:58:25 -0300

    Dashborard create, read

--- client/src/pages/BonosPage.jsx
+++ client/src/pages/BonosPage.jsx
@@ -1,10 +1,11 @@
 import { Button, Container } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BonosPage = () => {
   return (
     <Container maxWidth="lg" sx={{paddingY: '30px'}}>
-      <Button variant='contained' color='success' sx={{backgroundColor: 'rgb(63 98 18)'}}>Add New Bonds</Button>
+      <Link to='/bonos/new'><Button variant='contained' color='success' sx={{backgroundColor: 'rgb(63 98 18)'}}>Add New Bonds</Button></Link>
     </Container>
   );
 };
